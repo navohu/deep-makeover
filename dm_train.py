a2b92bf30363ce755268193db3493a3b9a5607a9
Tali <nathalievh95@gmail.com>
2017-12-19 19:56:09 +0000

    Adding write status

--- dm_train.py
+++ dm_train.py
@@ -86,7 +86,7 @@ def make_log_dir():
         new_number=max(file_nums)+1 #find largest and increment
 
     filename = '/log_%06d.csv' % (new_number)
-    with open(FLAGS.logs + filename, "a") as empty_csv:
+    with open(FLAGS.logs + filename, "w") as empty_csv:
         writer = csv.writer(f)
         writer.writerow(['Step', 'Generator', 'Discriminator', 'Real', 'Fake'])
 
